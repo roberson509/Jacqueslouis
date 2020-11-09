ed52062ebc119ede0f9ab9216883928221fe0149
roberson509 <42021918+roberson509@users.noreply.github.com>
2020-11-09 17:35:10 -0500

    position

--- menu.js
+++ menu.js
@@ -1,17 +1,13 @@
 var check = document.querySelector(".checkBtn");
-
-
 var mobilMenu = document.querySelector("ul");
-    var about = document.querySelector("#aboutMe");
-    var open = document.querySelector("#open");
+var about = document.querySelector("#aboutMe");
+var open = document.querySelector("#open");
 
 check.addEventListener("click",()=>{
-    
     mobilMenu.style.left = 0;
     about.style.display = "none";
     open.style.display = "none";
 })
-
 
 function tap(){
     about.style.display = "block";
