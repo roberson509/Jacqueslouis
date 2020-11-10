8c8ffee3fcd5fc891fdef56e7608d54cd59556ef
roberson509 <42021918+roberson509@users.noreply.github.com>
2020-11-10 14:54:51 -0500

    wraper

--- menu.js
+++ menu.js
@@ -1,16 +1,16 @@
 var check = document.querySelector(".checkBtn");
 var mobilMenu = document.querySelector("ul");
-var about = document.querySelector("#aboutMe");
+var wraper = document.querySelector("#wraper");
 var open = document.querySelector("#open");
 
 check.addEventListener("click",()=>{
     mobilMenu.style.left = 0;
-    about.style.display = "none";
+    wraper.style.display = "none";
     open.style.display = "block";
 })
 
 function tap(){
-    about.style.display = "block";
+    wraper.style.display = "block";
     mobilMenu.style.left = "-100%";
     open.style.display = "block";
     
